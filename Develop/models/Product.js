4da90c25753c5b10136af15695e2673f30c7a206
Maria Paterno <mariapaterno7@yahoo.com>
2022-03-18 16:20:58 -0600

    Updated models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -8,16 +8,48 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
+{ id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true, 
+    autoIncrement: true, 
+    allowNull: false
   },
+  product_name: {
+type: DataTypes.STRING,
+allowNull: false
+  },
+  price: {
+    type: DataTypes.DECIMAL(10,2),
+    allowNull: false,
+    validate: {
+      isDecimal: true
+    }
+  },
+  stock: {
+type: DataTypes.INTEGER, 
+defaultValue: 10, 
+allowNull: false, 
+validate: {
+  isNumeric: true
+}
+  },
+  category_id: {
+    type: DataTypes.INTEGER, 
+    references: {
+      model: 'category', 
+      key: 'id'
+    }
+}},
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product',
-  }
+  },
 );
 
 module.exports = Product;
+
+
+//set default values default value of 10
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -5,9 +5,15 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
+  { id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true, 
+    allowNull: false
   },
+  tag_name: {
+    type: DataTypes.STRING
+  }},
   {
     sequelize,
     timestamps: false,
