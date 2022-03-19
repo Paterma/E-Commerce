ca1ac0bba3b83eb2b874aa292032a071d671c257
Maria Paterno <mariapaterno7@yahoo.com>
2022-03-18 19:02:06 -0600

    Fixed one error

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_USER, process.env.DB_PW,process.env.DB_NAME, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
@@ -13,3 +13,27 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
+
+// require('dotenv').config();
+
+// const mysql = mysql2 require('mysql2');
+
+// // const sequelize = process.env.JAWSDB_URL
+// //   ? new Sequelize(process.env.JAWSDB_URL)
+// //   : new Sequelize(process.env.DB_USER, process.env.DB_PW,process.env.DB_NAME, {
+// //       host: 'localhost',
+// //       dialect: 'mysql',
+// //       dialectOptions: {
+// //         decimalNumbers: true,
+// //       },
+// //     });
+//     const sequelize = mysql.createConnection(
+//       {
+//           host: 'localhost',
+//           user: 'root',
+//           password: 'root',
+//           database: 'ecommerce_db'
+//     });
+
+// module.exports = sequelize;
+
